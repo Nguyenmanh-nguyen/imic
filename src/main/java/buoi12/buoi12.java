064f03e7f3dc44dd365e37c1ed21d3c4ba18b26b
acer@LAPTOP-ARMR41VM.TOTOLINK <acer@LAPTOP-ARMR41VM.TOTOLINK>
2019-04-19 19:52:23 +0700

    Buoi12

--- src/main/java/buoi12/buoi12.java
+++ src/main/java/buoi12/buoi12.java
@@ -9,6 +9,24 @@ public class buoi12
         }
     }
 
+    public static void inGiaTriTran(double[] a){
+        for(double i : a ){
+            System.out.println(Math.ceil(i));
+        }
+    }
+
+    public static void inGiaTriSan(double[] a){
+        for(double i : a ){
+            System.out.println(Math.floor(i));
+        }
+    }
+
+    public static void inGiaTriLamTron(double[] a){
+        for(double i : a ){
+            System.out.println(Math.round(i));
+        }
+    }
+
     public static double timMin(double[] a){
         double min = a[0];
         for(double i : a ){
@@ -22,5 +40,9 @@ public class buoi12
             if ( i > 0 ) System.out.println(Math.sqrt(i));
         }
     }
+    public static void inSoNgauNhien(double[] a){
+
+    }
+
 
 }
